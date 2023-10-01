cdef997258a4d12d21cc523b8088a34a2b6f1110
Chrisser1 <chrisgthomsen0310@gmail.com>
2023-10-01 15:19:11 +0200

    Adding a server
    Co-authored-by: Lukas <ItsLukV@users.noreply.github.com>

--- src/main/battleship/logic/Ship.java
+++ src/main/battleship/logic/Ship.java
@@ -1,5 +1,41 @@
 package main.battleship.logic;
 
 public class Ship {
-  
+  enum ShipType {
+    CARRIER (5),
+    BATTLESHIP (4),
+    CRUISER (3),
+    SUBMARINE (3),
+    DESTROYER (2);
+
+    private final int holes;
+
+    ShipType(int holes) {
+      this.holes = holes;
+    }
+
+    public int getHoles() {
+      return holes;
+    }
+  }
+
+  private final ShipType type;
+  private boolean isVertical;
+
+  public Ship(ShipType type) {
+    this.type = type;
+    this.isVertical = false;
+  }
+
+  public ShipType getType() {
+    return type;
+  }
+
+  public boolean isVertical() {
+    return isVertical;
+  }
+
+  public void changeRotation() {
+    this.isVertical = !this.isVertical;
+  }
 }
