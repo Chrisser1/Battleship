ca13791e6ef30e93b5df1c89ca60db8d16cc8a2b
dendersen <dendersen@endersen.dk>
2023-10-01 18:17:32 +0200

    fix?
    Co-authored-by: Lukas <ItsLukV@users.noreply.github.com>
    Co-authored-by: Chrisser1 <Chrisser1@users.noreply.github.com>

--- battleship/gui/Controller/draw/Art.java
+++
@@ -1,8 +0,0 @@
-// package main.battleship.gui.Controller.draw;
-
-// import processing.core.PGraphics;
-
-// abstract public interface Art {
-//   abstract public void draw(PGraphics g);
-//   abstract public void setPossition(int x,int y);
-// }
