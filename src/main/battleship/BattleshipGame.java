093354a2781c43520bd24d3087ef6048007df4e0
Chrisser1 <chrisgthomsen0310@gmail.com>
2023-10-01 15:56:45 +0200

    Server and client is working, need to be continues

--- src/main/battleship/BattleshipGame.java
+++ src/main/battleship/BattleshipGame.java
@@ -4,13 +4,29 @@ import main.battleship.server.*;
 
 public class BattleshipGame {
   public static void main(String[] args) {
-    BattleshipGame e = new BattleshipGame();
-    e.givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect();
+    // Start the server on a new thread
+    new Thread(() -> {
+            Server server = new Server();
+            server.start(6666);
+    }).start();
+
+    // Pause to ensure server starts before client tries to connect
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      e.printStackTrace();
+    }
+
+    BattleshipGame game = new BattleshipGame();
+    game.givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect();
   }
+  
   public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() {
+    // Assuming Client class and it's methods are correctly defined
     Client client = new Client();
     client.startConnection("127.0.0.1", 6666);
     String response = client.sendMessage("hello server");
-    System.out.println("hello client".equals(response));
+    System.out.println("'Hello client' received from server: " + "hello client".equals(response));
   }
 }
--- src/main/battleship/server/Server.java
+++ src/main/battleship/server/Server.java
@@ -14,22 +14,19 @@ public class Server {
     private BufferedReader in;
 
     public void start(int port) {
-        try {
-            serverSocket = new ServerSocket(port);
-            clientSocket = serverSocket.accept();
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            String greeting = in.readLine();
-            if ("hello server".equals(greeting)) {
-                out.println("hello client");
-            } else {
-                out.println("unrecognised greeting");
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+        if (".".equals(inputLine)) {
+            out.println("good bye");
+            break;
+         }
+        out.println(inputLine);
+    }
     }
 
     public void stop() {
@@ -49,10 +46,5 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        Server server=new Server();
-        server.start(6666);
     }
 }
